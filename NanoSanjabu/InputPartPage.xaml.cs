ec51bf4a17e7b8e01cf3a332d27bf7a9816262b3
chae-munho <ansgh4300@naver.com>
2026-02-19 10:27:50 +0900

    투입부 작업중1

--- NanoSanjabu/InputPartPage.xaml.cs
+++ NanoSanjabu/InputPartPage.xaml.cs
@@ -1,26 +1,45 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using System.Windows;
+﻿using System.Collections.Generic;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace NanoSanjabu
 {
-    /// <summary>
-    /// InputPartPage.xaml에 대한 상호 작용 논리
-    /// </summary>
     public partial class InputPartPage : Page
     {
+        public class TrayItem
+        {
+            public string TrayText { get; set; } = "";
+            public string StatusText { get; set; } = "";
+            public string TimeText { get; set; } = "";
+            public Brush StatusBrush { get; set; } = Brushes.Gray;
+        }
+
         public InputPartPage()
         {
             InitializeComponent();
+
+            // 테스트 데이터 50개 생성 (5x10)
+            var items = new List<TrayItem>();
+            for (int i = 1; i <= 50; i++)
+            {
+                // 예시로 1~3은 RUN, 4~6은 COMPLETE, 나머지는 STOP
+                Brush brush;
+                string status;
+
+                if (i <= 3) { brush = (Brush)FindResource("StatusRun"); status = "STATUS: RUN"; }
+                else if (i <= 6) { brush = (Brush)FindResource("StatusComplete"); status = "STATUS: COMPLETE"; }
+                else { brush = (Brush)FindResource("StatusStop"); status = "STATUS: STOP"; }
+
+                items.Add(new TrayItem
+                {
+                    TrayText = $"Tray {i}",
+                    StatusText = status,
+                    TimeText = "TIME: 0m",
+                    StatusBrush = brush
+                });
+            }
+
+            TrayItems.ItemsSource = items;
         }
     }
 }
